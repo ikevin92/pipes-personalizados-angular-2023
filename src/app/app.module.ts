280f59794a5a1508f6fb0cf162a34335658d8815
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-17 11:14:01 -0500

    add products routing and page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     ButtonModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
